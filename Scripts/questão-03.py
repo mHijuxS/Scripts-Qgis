4657d0a76f3ba96af0272ffcfd1975622bf524f7
mHiluxS <108304662+mHijuxS@users.noreply.github.com>
2023-03-19 14:01:33 -0300

    Adicionando o cabeçaclho

--- Scripts/questão-03.py
+++ Scripts/questão-03.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """
-***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************
+/***************************************************************************
+Lista de exercícios de Programação Aplicada
+Grupo 2
+Alunos:
+ - Al Ferreira
+ - Al Charles
+ - Al Nojima
+Solução Questão 3
+ ***************************************************************************/
 """
 
 from qgis.PyQt.QtCore import QCoreApplication
